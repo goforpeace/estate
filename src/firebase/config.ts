2bf62746f0b52fdf1efa45da09c8b754720111c2
Fazle Rifat Anonto <anonto.kings9@gmail.com>
2026-01-13 06:38:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-907320032-3bcaf",
+  "appId": "1:308878658176:web:c9ab94beae59c20302b465",
+  "apiKey": "AIzaSyAuSTpBud2BDXxKY5t2lQZzH_xTvrQjVVQ",
+  "authDomain": "studio-907320032-3bcaf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "308878658176"
+};
