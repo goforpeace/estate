5772f8ad21b2e514e3fd7e0fe17451fadb97f0cb
Fazle Rifat Anonto <anonto.kings9@gmail.com>
2026-01-14 17:01:08 +0000

    kindly add all the domains for image link

--- next.config.ts
+++ next.config.ts
@@ -12,28 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https'
-        ,
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'scontent.fdac135-1.fna.fbcdn.net',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
