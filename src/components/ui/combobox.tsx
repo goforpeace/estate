991db8dd073c0825acfdb997703c98fcdc02174f
Fazle Rifat Anonto <anonto.kings9@gmail.com>
2026-01-16 23:53:51 +0000

    still no luck

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -64,7 +64,7 @@ export function Combobox({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0">
+      <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0 z-[100]">
         <Command>
           <CommandInput placeholder={searchPlaceholder} />
           <CommandList>
